7606976830afb95eefe265bdccaa58bdf29f7fbd
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-06-19 12:01:56 +0100

    upadte sub

--- sub.c
+++ sub.c
@@ -4,7 +4,6 @@
  * _sub -  subtracts top element of the stack from the second top element
  * @head: double pointer to header (top) of the stack
  * @line_number: counter for line number of the file
- * Author: Excel Nwachukwu
  *
  * Return: void
  */
